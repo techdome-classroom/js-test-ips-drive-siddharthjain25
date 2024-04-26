a396d8954ed6d3cf2d8ebcee30b16c08bdbb286c
Siddharth Jain <83913594+siddharthjain25@users.noreply.github.com>
2024-04-26 13:26:18 +0530

    Add files via upload
    Final Test Cases

--- program1.js
+++ program1.js
@@ -1,9 +1,30 @@
 function smallestMissingPositiveInteger(nums) {
+  let n = nums.length;
 
-
-    // Implement the function smallest_missing_positive_integer
-  
+  for (let i = 0; i < n; i++) {
+      if (nums[i] <= 0 || nums[i] > n) {
+          nums[i] = 0;
+      }
   }
-  
-  module.exports = smallestMissingPositiveInteger;
 
+  for (let i = 0; i < n; i++) {
+      let num = Math.abs(nums[i]);
+      if (num !== 0) {
+          nums[num - 1] = -Math.abs(nums[num - 1]);
+      }
+  }
+
+  for (let i = 0; i < n; i++) {
+      if (nums[i] >= 0) {
+          return i + 1;
+      }
+  }
+
+  return n + 1;
+}
+
+console.log(smallestMissingPositiveInteger([3, 4, -1, 1]));
+console.log(smallestMissingPositiveInteger([1, 2, 0])); 
+console.log(smallestMissingPositiveInteger([-1, -3, 4, 2]));
+
+module.exports = smallestMissingPositiveInteger;
--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
 function longestSubstring(s) {
+    let longest = 0;
+    let start = 0;
+    const map = new Map();
 
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
 
-    // Implementation of longestSubstring function
+        if (map.has(char) && map.get(char) >= start) {
+            start = map.get(char) + 1;
+        }
+
+        map.set(char, i);
+        longest = Math.max(longest, i - start + 1);
+    }
+
+    return longest;
 }
+
+console.log(longestSubstring("abcabcbb"));
 
 module.exports = { longestSubstring };
